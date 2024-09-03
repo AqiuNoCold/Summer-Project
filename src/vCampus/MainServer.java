b8b95ba19b032d39b2a384c03060d183a068a90d
july <julywu399@gmail.com>
2024-09-03 15:26:41 +0800

    更新了前后端接口

--- src/vCampus/MainServer.java
+++ src/vCampus/MainServer.java
@@ -1,9 +1,9 @@
 package vCampus;
 
-import Pages.Pages.LibraryPage;
-import Pages.Pages.LoginPage;
-import Pages.Pages.StorePage;
+import vCampus.Dao.Criteria.*;
 import vCampus.Entity.User;
+import vCampus.Entity.Books.*;
+import vCampus.Service.*;
 import vCampus.User.IUserServerSrv;
 
 import java.io.IOException;
@@ -17,7 +17,6 @@ public class MainServer {
     private static final int PORT = 5101;
     private static final int MAX_CONNECTIONS = 10;
     private static ConcurrentHashMap<Socket, Thread> socketMap = new ConcurrentHashMap<>();
-
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
@@ -46,12 +45,11 @@ public class MainServer {
 
     private static void handleClient(Socket clientSocket) {
         try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-             ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
             while (true) {
-                String Model = (String) in.readObject();
-                // 不断读取客户端发送的对象
+                String model = (String) in.readObject();
                 String function = (String) in.readObject();
-                switch (Model){
+                switch (model) {
                     case "1":
                         LoginPage(function, in, out);
                         break;
@@ -81,6 +79,81 @@ public class MainServer {
         }
     }
 
+    private static void LibraryPage(String function, ObjectInputStream in, ObjectOutputStream out)
+            throws IOException, ClassNotFoundException {
+        LibraryService libraryService = new LibraryService();
+        switch (function) {
+            case "login":
+                String userId = (String) in.readObject();
+                BookUser bookUser = libraryService.login(userId);
+                out.writeObject(bookUser);
+                break;
+            case "borrowBook":
+                BookUser borrower = (BookUser) in.readObject();
+                String bookId = (String) in.readObject();
+                BorrowRecord borrowRecord = libraryService.borrowBook(borrower, bookId);
+                out.writeObject(borrowRecord);
+                break;
+            case "returnBook":
+                BorrowRecord returnRecord = (BorrowRecord) in.readObject();
+                BorrowRecord returnedRecord = libraryService.returnBook(returnRecord);
+                out.writeObject(returnedRecord);
+                break;
+            case "searchBooks":
+                BookSearchCriteria searchCriteria = (BookSearchCriteria) in.readObject();
+                BookSortCriteria sortCriteria = (BookSortCriteria) in.readObject();
+                int page = (int) in.readObject();
+                int pageSize = (int) in.readObject();
+                SearchResult<Book> searchResult = libraryService.searchBooks(searchCriteria, sortCriteria, page,
+                        pageSize);
+                out.writeObject(searchResult);
+                break;
+            case "getTotalBooks":
+                BookSearchCriteria totalSearchCriteria = (BookSearchCriteria) in.readObject();
+                int totalBooks = libraryService.getTotalBooks(totalSearchCriteria);
+                out.writeObject(totalBooks);
+                break;
+            case "createBookShelf":
+                BookUser shelfUser = (BookUser) in.readObject();
+                String shelfName = (String) in.readObject();
+                BookUser updatedUser = libraryService.createBookShelf(shelfUser, shelfName);
+                out.writeObject(updatedUser);
+                break;
+            case "setCurrentBookShelf":
+                BookUser currentUser = (BookUser) in.readObject();
+                BookShelf currentShelf = (BookShelf) in.readObject();
+                BookUser updatedCurrentUser = libraryService.setCurrentBookShelf(currentUser, currentShelf);
+                out.writeObject(updatedCurrentUser);
+                break;
+            case "addBookToShelfById":
+                BookUser addUser = (BookUser) in.readObject();
+                Long addShelfId = (Long) in.readObject();
+                String addBookId = (String) in.readObject();
+                BookUser updatedAddUser = libraryService.addBookToShelfById(addUser, addShelfId, addBookId);
+                out.writeObject(updatedAddUser);
+                break;
+            case "addBookToShelfByObject":
+                BookUser addObjectUser = (BookUser) in.readObject();
+                Long addObjectShelfId = (Long) in.readObject();
+                Book addObjectBook = (Book) in.readObject();
+                BookUser updatedAddObjectUser = libraryService.addBookToShelfByObject(addObjectUser, addObjectShelfId,
+                        addObjectBook);
+                out.writeObject(updatedAddObjectUser);
+                break;
+            case "removeBookFromShelfById":
+                BookUser removeUser = (BookUser) in.readObject();
+                Long removeShelfId = (Long) in.readObject();
+                String removeBookId = (String) in.readObject();
+                BookUser updatedRemoveUser = libraryService.removeBookFromShelfById(removeUser, removeShelfId,
+                        removeBookId);
+                out.writeObject(updatedRemoveUser);
+                break;
+            default:
+                System.out.println("Unknown function: " + function);
+                break;
+        }
+        out.flush();
+    }
 
     private static void LoginPage(String function, ObjectInputStream in, ObjectOutputStream out)
             throws IOException, ClassNotFoundException {
@@ -97,7 +170,7 @@ public class MainServer {
                 case "Forget":
                     String userId = (String) in.readObject();
                     String email = (String) in.readObject();
-                    User finduser = IUserServerSrv.forgetPassword(userId,email);
+                    User finduser = IUserServerSrv.forgetPassword(userId, email);
                     out.writeObject(finduser);
                     break;
             }
@@ -105,7 +178,6 @@ public class MainServer {
             System.out.println("Unknown function: " + function);
         }
     }
-
 
     private static void CoursePage(String function, ObjectInputStream in, ObjectOutputStream out)
             throws IOException, ClassNotFoundException {
@@ -119,17 +191,6 @@ public class MainServer {
     }
 
     private static void EcardPage(String function, ObjectInputStream in, ObjectOutputStream out)
-            throws IOException, ClassNotFoundException {
-        if (function != null) {
-            switch (function) {
-                case "Course":
-            }
-        } else {
-            System.out.println("Unknown function: " + function);
-        }
-    }
-
-    private static void LibraryPage(String function, ObjectInputStream in, ObjectOutputStream out)
             throws IOException, ClassNotFoundException {
         if (function != null) {
             switch (function) {
